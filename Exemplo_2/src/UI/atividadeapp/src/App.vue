c267acd9a3970b70a8127536a1ccbc48c7432013
Marlom Souza <marlomsouza@digix.com.br>
2018-12-13 13:26:56 -0300

    testando vue

--- Exemplo_2/src/UI/atividadeapp/src/App.vue
+++ Exemplo_2/src/UI/atividadeapp/src/App.vue
@@ -2,12 +2,29 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view/>
+    <button type="button" class="" @click="printarLog()">Printar log</button>
+
+    <input type="text" v-model="this.nome">
+    <label>{{nome}}</label>
+
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  created() {
+    console.log('#################fui criado')
+  },
+  methods: {
+    printarLog() {
+       setTimeout(() => { this.nome = "alterado depois de 2 segundos!" }, 2000)
+       this.nome = "marlim"
+    }
+  },
+  data() {
+      return { nome: "Marlom"}
+  },
 }
 </script>
 
